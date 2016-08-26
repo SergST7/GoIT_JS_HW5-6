37ddd35aac7c284341d7cc01dc747f8c0e3d3d36
Sergey Stelmakh <stelmakhserg@gmail.com>
2016-08-26 23:21:53 +0300

    reformat code

--- js/main.js
+++ js/main.js
@@ -1,84 +1,91 @@
 /**
  * Created by SergST on 25.08.2016.
  */
-var startButton = document.getElementById('start-btn');
-startButton.addEventListener('click', start);
-
-var stopButton = document.getElementById('reset-btn');
-stopButton.addEventListener('click', reset);
-
-var splitButton = document.getElementById('split-btn');
-splitButton.addEventListener('click', getSplit);
-
-var startTime, timerId;
 
 var timerBox = document.getElementById('timer-box');
 var splitBox = document.getElementById('split-box');
 
-var timerStatus = 0; //0-пауза или стоп, 1 - работает
-var timeDifferensMs;
-var timerPauseValue = 0;
+var timer = {
 
-function start(){
-  if (startButton.value == 'start'){
-    timerStatus = 1;
-    startButton.value = 'pause';
-    startTime = new Date();
-    findTime();
-    timerId = setInterval(findTime, 31);
-  }
-  else {
-    stop();
+  startTime: 0,          //время начала отсчета
+  timeDifferensMs: 0,    //разница ms между началом отсчета и текущим временем
+  timerId: 0,            //ид для отмены интервала
+  status: 0,              //0-пауза или стоп, 1 - работает
+  pauseValue: 0,          //храним значения таймера в момент паузы
+
+  start: function () {
+    if (startButton.value == 'start') {
+      timer.status = 1;
+      startButton.value = 'pause';
+      timer.startTime = new Date();
+      timer.findTime();
+      timer.timerId = setInterval(timer.findTime, 31);
+    }
+    else {
+      timer.stop();
+      startButton.value = 'start';
+    }
+  },
+
+  findTime: function() {
+    var deltaTime = new Date();
+    timer.timeDifferensMs = deltaTime.getTime() - timer.startTime.getTime() + timer.pauseValue;
+    timerBox.innerHTML = timer.getTimeFormat(timer.timeDifferensMs);
+  },
+
+  getTimeFormat: function (t) {     //преобразует число мс в строку чч:мм:сс:мс
+    var ms = t % 1000;
+    t -= ms;
+    t = Math.floor(t / 1000);
+    var sec = t % 60;
+    t -= sec;
+    t = Math.floor(t / 60);
+    var min = t % 60;
+    t -= min;
+    t = Math.floor(t / 60);
+    var h = t % 60;
+    if (ms < 10)  ms = '00' + ms;
+    else if (ms < 100)  ms = '0' + ms;
+    if (sec < 10)  sec = '0' + sec;
+    if (min < 10)  min = '0' + min;
+    if (min < 10)  h = '0' + h;
+
+    return h + ' : ' + min + ' : ' + sec + ' : ' + ms;
+  },
+
+  getSplit: function () {
+    if (timer.status) {
+      splitBox.innerHTML += '<p> Split: ' + timer.getTimeFormat(timer.timeDifferensMs) + '</p>';
+    }
+  },
+
+  stop: function () {
+    clearInterval(timer.timerId);
+    if (timer.status) {
+      splitBox.innerHTML += '<p> Stop: ' + timer.getTimeFormat(timer.timeDifferensMs) + '</p>';
+    }
+    timer.pauseValue = this.timeDifferensMs;
+    timer.status = 0;
+  },
+
+  reset: function () {
+    clearInterval(timer.timerId);
+    timerBox.innerHTML = '00 : 00 : 00 : 000';
+    splitBox.innerHTML = '';
+    timer.status = 0;
+    timer.timeDifferensMs = 0;
+    timer.pauseValue = 0;
     startButton.value = 'start';
   }
-}
+};
 
-function findTime(){
-  var deltaTime = new Date();
-  timeDifferensMs =  deltaTime.getTime() - startTime.getTime() + timerPauseValue;
-  timerBox.innerHTML = getTimeFormat(timeDifferensMs);
-}
+var startButton = document.getElementById('start-btn');
+startButton.addEventListener('click', timer.start);
 
-function getTimeFormat(t){ //преобразует число мс в строку чч:мм:сс:мс
-  var ms = t%1000;
-  t -= ms;
-  t = Math.floor(t/1000);
-  var sec = t%60;
-  t -= sec;
-  t = Math.floor(t/60);
-  var min = t%60;
-  t -= min;
-  t = Math.floor(t/60);
-  var h = t%60;
-  if (ms < 10) {ms = '00' + ms} 
-  else if (ms < 100) {ms = '0' + ms}
-  if (sec < 10) {sec = '0' + sec}
-  if (min < 10) {min = '0' + min}
-  if (min < 10) {h = '0' + h}
-  return h + ' : ' + min + ' : ' + sec + ' : ' + ms;
-}
+var stopButton = document.getElementById('reset-btn');
+stopButton.addEventListener('click', timer.reset);
 
-function getSplit(){
-  if (timerStatus){
-    splitBox.innerHTML += '<p> Split: ' + getTimeFormat(timeDifferensMs) + '</p>';
-  }
-}
+var splitButton = document.getElementById('split-btn');
+splitButton.addEventListener('click', timer.getSplit);
 
-function stop(){
-  clearInterval(timerId);
-  if (timerStatus){
-    splitBox.innerHTML += '<p> Stop: ' + getTimeFormat(timeDifferensMs) + '</p>';
-  }
-  timerPauseValue = timeDifferensMs;
-  timerStatus = 0;
-}
-
-function reset(){
-  clearInterval(timerId);
-  timerBox.innerHTML = '00 : 00 : 00 : 000';
-  splitBox.innerHTML = '';
-  timerStatus  = 0;
-  timeDifferensMs = 0;
-  timerPauseValue = 0;
-  startButton.value = 'start';
-}
+timer.reset();
